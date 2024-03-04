99ee02b3f79b0c85d18ef6c5d1d4c888e85c3df0
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2024-03-05 12:13:17 +1300

    build(javascript): moved route files into v1 and created auth.js with some pasted code

--- routes/v1/quiz.js
+++ routes/v1/quiz.js
@@ -1,7 +1,7 @@
 //operations relating to quiz information depending on user role
 //e.g. getting user quiz or updating info, etc
 import express from "express"
-import * as resources from "../controllers/resources.js"
+import * as resources from "../../controllers/resources.js"
 
 const router = express.Router()
 
--- routes/v1/score.js
+++ routes/v1/score.js
@@ -1,7 +1,7 @@
 //operations relating to score information depending on user role
 //e.g. getting score info or updating info, etc
 import express from "express"
-import * as resources from "../controllers/resources.js"
+import * as resources from "../../controllers/resources.js"
 
 const router = express.Router()
 
