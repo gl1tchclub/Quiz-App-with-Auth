cc75c370ee6869afb4fa55c60611b26b0f5945f9
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2024-03-07 11:45:00 +1300

    fix(js): fixed import

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,7 +1,7 @@
 //operations relating to user information depending on user role
 //e.g. getting user info or updating info, etc
 import express from "express"
-import * as resources from "../../controllers/resources.js"
+import * as resources from "../../controllers/v1/resources.js"
 
 const router = express.Router()
 
