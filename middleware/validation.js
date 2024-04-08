a92dc0b35e7cd4149b165f2db00b5307b869fcfc
mintep1-student <mintep1@student.op.ac.nz>
2024-04-09 11:48:02 +1200

    feat: adding registering user validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -2,4 +2,26 @@
  * @file validation functions for various data entities
  * @author Elizabeth Minty
  */
-import Joi from "joi"
+import Joi from "joi";
+
+const validateRegister = (req, res, next) => {
+  const userSchema = Joi.object({
+    email: Joi.string().min(7).max(50).messages({
+      "string.base": "Email must be a string",
+      "string.min": "Email must have a minimum length of {#limit}",
+      "string.max": "Email must have a maximum length of {#limit}",
+    }),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+
+    repeat_password: Joi.ref("password"),
+  });
+  const { error } = userSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      msg: error.details[0].message,
+    });
+  }
+
+  next();
+};
