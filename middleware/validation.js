d1a4bed19f51f5796e87a7eef70b059b7bfff1ec
mintep1-student <mintep1@student.op.ac.nz>
2024-04-09 17:30:01 +1200

    feat: added validation checks in register for names and password

--- middleware/validation.js
+++ middleware/validation.js
@@ -6,12 +6,21 @@ import Joi from "joi";
 
 const validateRegister = (req, res, next) => {
   const userSchema = Joi.object({
-    email: Joi.string().min(7).max(50).messages({
+    email: Joi.string().email().maxDomainSegments(2).required().messages({
       "string.base": "Email must be a string",
       "string.min": "Email must have a minimum length of {#limit}",
       "string.max": "Email must have a maximum length of {#limit}",
     }),
-    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+    firstName: Joi.string().min(2).max(50).required().pattern(new RegExp("^[a-zA-Z]$")).messages({
+
+    }),
+    lastName: Joi.string().min(2).max(50).required().pattern(new RegExp("^[a-zA-Z]$")).messages({
+
+    }),
+    username: Joi.string().min(5).max(10).alphanum().required().messages({
+
+    }),
+    password: Joi.string().required().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).min(8).max(16).contains(),
 
     repeat_password: Joi.ref("password"),
   });
