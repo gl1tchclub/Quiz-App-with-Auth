f67f6833608f4b8ebbd0666f0f0d301a97feaaad
mintep1-student <mintep1@student.op.ac.nz>
2024-04-16 18:41:30 +1200

    chore: add crud to quiz route, remove unnecessary files

--- routes/v1/quiz.js
+++ routes/v1/quiz.js
@@ -1,18 +1,20 @@
 //operations relating to quiz information depending on user role
 //e.g. getting user quiz or updating info, etc
 import express from "express"
-import * as resources from "../../controllers/resources.js"
+import * as resources from "../../controllers/quiz.js"
 
 const router = express.Router()
 
 //POST
+router.post("/", (req, res) => resources.createQuiz(req, res));
 
 //GET ALL
+router.get("/", (req, res) => resources.getQuizzes(req, res));
 
-//GET ID
-
-//PUT
+//GET QUIZ SCORE DATA BY ID
+router.get("/:id", (req, res) => resources.getQuizScores(req, res));
 
 //DELETE
+router.delete("/:id", (req, res) => resources.deleteQuiz(req, res));
 
 export default router
