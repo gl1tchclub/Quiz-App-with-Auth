b64bc8c707a035d787444f66a6462856a05132a5
mintep1-student <mintep1@student.op.ac.nz>
2024-04-17 18:56:33 +1200

    feat: fetch quiz data, ask user to enter quiz data

--- controllers/v1/quiz.js
+++ controllers/v1/quiz.js
@@ -1,18 +1,46 @@
 import { PrismaClient } from "@prisma/client";
-// import { checkPrivilege } from "./resources.js";
 
 const prisma = new PrismaClient();
 
 const createQuiz = async (req, res) => {
-  const { id } = req.user;
+  try {
+    const { id } = req.user;
+    const user = await prisma.user.findUnique({ where: { id: id } });
 
-  const user = await prisma.user.findUnique({ where: { id: id } });
+    if (user.role == "BASIC_USER") {
+      return res.status(403).json({
+        msg: "Not authorized to create a quiz",
+      });
+    }
 
-  if (user.role == "BASIC_USER") {
-    return res.status(403).json({
-      msg: "Not authorized to create a quiz",
+    const { name, startDate, endDate } = req.body;
+
+    let quiz = await prisma.user.findUnique({
+      where: { name: name },
+    });
+
+    if (quiz)
+      return res
+        .status(409)
+        .json({ msg: `Quiz with name ${name} already exists` });
+    
+    if (Object.keys(req.body).length < 3) return res
+        .status(400)
+        .json({ msg: `Please fill out all fields` })
+
+    // Chooses a random category Id from 9-32 (change this to allow admin to select category)
+    let catId = Math.floor(Math.random() * 32) + 9
+    let res = await fetch("https://opentdb.com/api.php?amount=10");
+    let quizData = await res.json();
+    quiz = await prisma.quiz.create({
+        data: {
+            categoryId: catId
+        }
+    })
+
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
     });
   }
-
-  
 };
--- prisma/data/02-categorySeed.js
+++ prisma/data/02-categorySeed.js
@@ -2,7 +2,7 @@
  * @file Seeds data for the admin users and inserts it into the User table
  * @author Elizabeth Minty
  */
-const name = "user";
+const name = "category";
 const data = async () => {
     let res = await fetch("https://opentdb.com/api_category.php");
     let json = await res.json();
