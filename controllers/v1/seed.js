81d05bf521fc0736dec59ea819af8813a380bd70
mintep1-student <mintep1@student.op.ac.nz>
2024-04-30 11:33:32 +1200

    style

--- controllers/v1/seed.js
+++ controllers/v1/seed.js
@@ -3,8 +3,19 @@ import bcryptjs from "bcryptjs";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const seedBasicUsers = async (req, res) => {
-    try {
-        const { id } = req.user;
-    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+  try {
+    const { id } = req.user;
+    const user = await prisma.user.findUnique({ where: { id: id } });
+    if (user.role == "BASIC_USER") {
+      return res.status(403).json({
+        msg: "Not authorized to access this route",
+      });
     }
-}
+
+    let { name, data } = await import("../../prisma/data/03-basicSeed");
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
