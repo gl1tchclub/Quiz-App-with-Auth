a5e0f58247aa8ac4d143d6a9ee86e31886b3eb6f
mintep1-student <mintep1@student.op.ac.nz>
2024-05-16 17:59:15 +1200

    feat: start getAnswer function

--- controllers/v1/answer.js
+++ controllers/v1/answer.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const createQuestionAnswer = async (req, res) => {
+const createAnswer = async (req, res) => {
   try {
     const { userId } = req.user;
     const user = await prisma.user.findUnique({ where: { id: userId } });
@@ -50,3 +50,36 @@ const createQuestionAnswer = async (req, res) => {
     });
   }
 };
+
+const getAnswer = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+      return res.status(404).json({ msg: `No user found with ID ${userId}` });
+    }
+    const { quizId, questionId, answer } = req.body;
+
+    const quiz = await prisma.quiz.findUnique({
+      where: { id: quizId },
+    });
+
+    // Ensure quiz exists
+    if (!quiz)
+      return res.status(404).json({ msg: `No quiz found with ID ${quizId}` });
+
+    const question = quiz.questions.find((q) => q.id === questionId);
+
+    // Check that question exists in given quiz
+    if (!question)
+      return res
+        .status(404)
+        .json({ msg: `No question found with ID ${questionId} in this quiz` });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
+
+export{ createAnswer, getAnswer };
