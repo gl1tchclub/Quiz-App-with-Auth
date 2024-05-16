094565d981f21a93ec0434a59eaf3387a230e0bf
mintep1-student <mintep1@student.op.ac.nz>
2024-05-16 17:38:34 +1200

    chore: comments

--- controllers/v1/answer.js
+++ controllers/v1/answer.js
@@ -4,28 +4,31 @@ const prisma = new PrismaClient();
 
 const createQuestionAnswer = async (req, res) => {
   try {
-    const { id } = req.user;
+    const { userId } = req.user;
     const { quizId, questionId, answer } = req.body;
 
     const quiz = await prisma.quiz.findUnique({
       where: { id: quizId },
     });
 
+    // Ensure quiz exists
     if (!quiz)
       return res.status(404).json({ msg: `No quiz found with ID ${quizId}` });
 
     const questionObj = quiz.questions.find((q) => q.id === questionId);
 
+    // Check that question exists in given quiz
     if (!questionObj)
       return res
         .status(404)
         .json({ msg: `No questions found with ID ${questionId}` });
 
+    // Set isCorrect by comparing given answer to correct answer
     const isCorrect = answer === questionObj.correctAnswer ? true : false;
 
     const userAnswer = await prisma.userQuestionAnswer.create({
       data: {
-        id,
+        userId,
         quizId,
         questionId,
         answer,
