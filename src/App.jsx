2db46d458df92cfd78dde3da1aa2467b1e38883c
mintep1-student <mintep1@student.op.ac.nz>
2024-06-01 16:52:33 +1200

    fixing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+// import "./index.css"
 import { useForm } from "react-hook-form";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import Navigation from "./components/Navigation";
