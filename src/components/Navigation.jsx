f9ddfca3cb422bd445489da08fe57f99f2636aa3
mintep1-student <mintep1@student.op.ac.nz>
2024-06-04 10:16:23 +1200

    refactor: create home page file for modularity

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,91 +10,82 @@ import Navbar from "./Nav";
 import Test from "./forms/TestForm";
 import RegisterForm from "./forms/RegisterForm";
 import LoginForm from "./forms/LoginForm";
-import CardWrapper from "./CardWrapper";
+
+
+import HomePage from "../pages/Home";
 
 const Navigation = () => {
   return (
     <Router>
-        <Navbar />
-        <section class="container mx-auto h-screen">
-          <div class=" flex items-center justify-center">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <CardWrapper
-                    title="Welcome to the Quiz App"
-                    variant="outline"
-                    href="/welcome"
-                    label="Made by Mintep1"
-                    buttonLabel="Click to get started!"
-                  ></CardWrapper>
-                }
-              ></Route>
-              <Route
-                path="/register"
-                element={
-                  <>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                      <RegisterForm />
-                      {/* <LoginForm /> */}
-                    </div>
-                  </>
-                }
-              />
-              <Route
-                path="/login"
-                element={
-                  <>
-                    <div
-                      style={{ display: "flex", justifyContent: "center" }}
-                    ></div>
-                  </>
-                }
-              />
-              <Route
-                path="/test"
-                element={
-                  <>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                      <Test />
-                    </div>
-                  </>
-                }
-              />
-              <Route
-                path="/quizzes"
-                element={
-                  <>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                      Quizzes
-                    </div>
-                  </>
-                }
-              />
-              <Route
-                path="/users"
-                element={
-                  <>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                      Users
-                    </div>
-                  </>
-                }
-              />
-              <Route
-                path="/user"
-                element={
-                  <>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                      User
-                    </div>
-                  </>
-                }
-              />
-            </Routes>
-          </div>
-        </section>
+      <Navbar />
+      <section className="container mx-auto h-screen">
+        <div className=" flex items-center justify-center">
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route
+              path="/register"
+              element={
+                <>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <RegisterForm />
+                    {/* <LoginForm /> */}
+                  </div>
+                </>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <>
+                  <div
+                    style={{ display: "flex", justifyContent: "center" }}
+                  ></div>
+                </>
+              }
+            />
+            <Route
+              path="/test"
+              element={
+                <>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Test />
+                  </div>
+                </>
+              }
+            />
+            <Route
+              path="/quizzes"
+              element={
+                <>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    Quizzes
+                  </div>
+                </>
+              }
+            />
+            <Route
+              path="/users"
+              element={
+                <>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    Users
+                  </div>
+                </>
+              }
+            />
+            <Route
+              path="/user"
+              element={
+                <>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    User
+                  </div>
+                </>
+              }
+            />
+          </Routes>
+        </div>
+      </section>
     </Router>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import CardWrapper from "../components/CardWrapper";
+
+const HomePage = () => {
+  return (
+    <CardWrapper
+      title="Welcome to the Quiz App"
+      variant="outline"
+      href="/test"
+      label="Made by Mintep1"
+      buttonLabel="Click to get started!"
+    ></CardWrapper>
+  );
+};
+export default HomePage;
