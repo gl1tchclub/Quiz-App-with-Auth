e56cd7a5d5cd9026bdbdf5b691ddc43e518c571d
mintep1-student <mintep1@student.op.ac.nz>
2024-06-11 17:24:46 +1200

    refactor: imrpove css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import LinkButton from "../components/buttons/LinkButton";
 
 const HomePage = () => {
   return (
-    <div className="w-3/5 h-dvh py-6 flex items-center justify-center">
+    <div className="w-full h-dvh flex items-center justify-center">
       <CardWrapper
         title="Welcome to the Quiz App"
         label="Made by Mintep1"
-        box="justify-center items-center"
+        box="justify-center items-center w-1/2 mx-auto bg-pink-300 shadow-lg rounded-lg p-6 mb-40"
       >
         <div className="flex justify-between">
           <LinkButton
