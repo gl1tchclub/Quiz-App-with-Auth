b8f322d6b199c29476fedaabee6dec72ee86cf89
mintep1-student <mintep1@student.op.ac.nz>
2024-06-11 17:41:08 +1200

    fix: logout button css on logout page centered

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -3,20 +3,18 @@ import React from "react";
 import CardWrapper from "../components/CardWrapper";
 
 const LogoutPage = () => {
-
   return (
     <div className="w-full h-dvh flex items-center justify-center">
       <CardWrapper
         title="Do you wish to log out?"
         label="We'll see you soon!"
-        box="justify-center items-center align-center w-1/2 mx-auto bg-pink-300 shadow-lg rounded-lg p-6 mb-40"
+        box="w-1/2 bg-pink-300 shadow-lg rounded-lg p-6 mb-40"
       >
-        
-        <LogoutButton />
-
+        <div className="flex justify-center mt-6">
+          <LogoutButton />
+        </div>
       </CardWrapper>
     </div>
-  
-  )
+  );
 };
 export default LogoutPage;
