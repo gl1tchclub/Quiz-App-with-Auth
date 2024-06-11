e0557bf3dc95cee24c8ac3b7279836c008b3080a
mintep1-student <mintep1@student.op.ac.nz>
2024-06-11 17:22:02 +1200

    feat: add docs button to home page and improve css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,27 @@
 import CardWrapper from "../components/CardWrapper";
+import LinkButton from "../components/buttons/LinkButton";
 
 const HomePage = () => {
   return (
     <div className="w-3/5 h-dvh py-6 flex items-center justify-center">
-    <CardWrapper
-      title="Welcome to the Quiz App"
-      variant="outline"
-      href="/register"
-      label="Made by Mintep1"
-      buttonLabel="Click to get started!"
-      button="true"
-      buttonStyle="bg-pink-700 text-white font-bold py-2 px-4 rounded hover:bg-pink-800"
-    ></CardWrapper>
+      <CardWrapper
+        title="Welcome to the Quiz App"
+        label="Made by Mintep1"
+        box="justify-center items-center"
+      >
+        <div className="flex justify-between">
+          <LinkButton
+            href="/register"
+            buttonLabel="Get Started!"
+            buttonStyle="bg-pink-600 text-white font-bold py-2 px-14 rounded-lg hover:bg-pink-400"
+          />
+          <LinkButton
+            href="/info"
+            buttonLabel="Docs"
+            buttonStyle="bg-pink-600 text-white font-bold py-2 px-20 rounded-lg hover:bg-pink-400"
+          />
+        </div>
+      </CardWrapper>
     </div>
   );
 };
