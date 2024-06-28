b51303c90d6967790d73fe4ffda4dfb82fb7969b
mintep1-student <mintep1@student.op.ac.nz>
2024-06-28 16:41:19 +1200

    feat: create quiz form mutation fn finished

--- src/components/forms/CreateQuizForm.jsx
+++ src/components/forms/CreateQuizForm.jsx
@@ -1,68 +1,93 @@
-const CreateQuiz = () => {
-    const createQuizForm = useForm();
-    const { mutate: postQuizMutation, data: quizData } = useMutation({
-        mutationFn: (user) =>
-          fetch("http://localhost:3000/api/v1/quiz", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              email: user.email,
-              password: user.password,
-            }),
-          }).then((res) => {
-            if (res.status === 200) {
-                createQuizForm.reset((formValues) => ({
-                ...formValues,
-                email: "",
-                password: "",
-              }));
-            }
-            return res.json();
-          }),
-        onSuccess: (data) => {
-        //   localStorage.setItem("token", data.token);
-        //   queryClient.invalidateQueries("userData");
-        },
-      });
-    
-      const handleCreateQuizSubmit = (values) => postQuizMutation(values);
-    
-      return (
-        <>
-          <Navigation/>
-          <h2>Quiz</h2>
-          <form onSubmit={createQuizForm.handleSubmit(handleCreateQuizSubmit)}>
-            {/* change to radio buttons */}
-            <label htmlFor="quiz-question">Email</label>
-            <input
-              type="text"
-              id="quiz-email"
-              name="email"
-              {...createQuizForm.quiz("email")}
-            />
-            <label htmlFor="quiz-answer">Password</label>
-            <input
-              type="radio"
-              id="quiz-answer"
-              name="answer"
-              {...createQuizForm.quiz("answer")}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{quizData?.msg}</p>
-    
-          <button
-            onClick={() => {
-              localStorage.removeItem("token");
-              // queryClient.invalidateQueries("institutionData");
-            }}
-          >
-            Logout
-          </button>
-        </>
-      );
-}
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import CardWrapper from "../CardWrapper";
+import { useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
-  export default CreateQuiz;
+const CreateQuiz = ({ refetch }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const createQuizForm = useForm();
+  const { mutate: postQuizMutation, data: quizData } = useMutation({
+    mutationFn: (quiz) =>
+      fetch("https://two4-mintep1-app-dev.onrender.com/api/v1/quizzes/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          categoryId: quiz.categoryId,
+          name: quiz.name,
+          type: quiz.type,
+          difficulty: quiz.difficulty,
+          startDate: quiz.startDate,
+          endDate: quiz.endDate,
+        }),
+      }).then((res) => {
+        if (res.status === 201) {
+          createQuizForm.reset((formValues) => ({
+            ...formValues,
+            categoryId: "",
+            name: "",
+            type: "",
+            difficulty: "",
+            startDate: "",
+            endDate: "",
+          }));
+        }
+        return res.json();
+      }),
+    onSuccess: (data) => {
+      if (!data.error) refetch();
+    },
+  });
+
+  const handleCreateQuizSubmit = (values) => postQuizMutation(values);
+
+  return (
+    <>
+      <Navigation />
+      <h2>Quiz</h2>
+      <form onSubmit={createQuizForm.handleSubmit(handleCreateQuizSubmit)}>
+        {/* change to radio buttons */}
+        <label htmlFor="quiz-question">Email</label>
+        <input
+          type="text"
+          id="quiz-email"
+          name="email"
+          {...createQuizForm.quiz("email")}
+        />
+        <label htmlFor="quiz-answer">Password</label>
+        <input
+          type="radio"
+          id="quiz-answer"
+          name="answer"
+          {...createQuizForm.quiz("answer")}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{quizData?.msg}</p>
+
+      <button
+        onClick={() => {
+          localStorage.removeItem("token");
+          // queryClient.invalidateQueries("institutionData");
+        }}
+      >
+        Logout
+      </button>
+    </>
+  );
+};
+
+export default CreateQuiz;
