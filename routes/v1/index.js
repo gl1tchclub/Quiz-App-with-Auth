fbbf56c8bf6fe5028bdc79ffdfc0cb68ff949d2a
Liz <132317396+mintep1-student@users.noreply.github.com>
2024-06-30 12:57:05 +1200

    Update index.js

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -4,20 +4,13 @@
  */
 //create router using express, import CRUD from controllers, create/export CRUD routes using router
 import express from "express";
-import * as score from "../../controllers/v1/score.js";
-import * as quiz from "../../controllers/v1/quiz.js";
+import { getIndex } from "../controllers/v1/index.js";
 
 // Create an Express router
 const router = express.Router();
 
 //GET ALL
-router.get("/all", (req, res) => quiz.getQuizzes(req, res));
-
-//GET BY ID
-router.get("/:id", (req, res) => quiz.getQuiz(req, res));
-
-//GET SCORES
-router.get("/scores/:type", (req, res) => score.getQuizScores(req, res));
+router.get("/", getIndex);
 
 // Export the router
 export default router;
