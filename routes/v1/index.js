68f67cf11ee553d8da9295791a8b05964f41cd6b
Liz <132317396+mintep1-student@users.noreply.github.com>
2024-06-30 13:12:28 +1200

    feat: index route displays all routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,15 +2,30 @@
  * @file Manages routes related to the root index page
  * @author Elizabeth Minty
  */
-//create router using express, import CRUD from controllers, create/export CRUD routes using router
 import express from "express";
-import { getIndex } from "../controllers/v1/index.js";
 
-// Create an Express router
 const router = express.Router();
 
-//GET ALL
-router.get("/", getIndex);
+router.get("/", (req, res) => {
+  let x = [
+    "Welcome! Here are all the available routes",
+    "Home: https://two4-mintep1-app-dev.onrender.com/api/v1/",
+    "Get Quizzes (no auth): https://two4-mintep1-app-dev.onrender.com/api/v1/public/all",
+    "Get Quiz Average Score (no auth): https://two4-mintep1-app-dev.onrender.com/api/v1/public/:id",
+    "Seed basic users (admin only): https://two4-mintep1-app-dev.onrender.com/api/v1/seedBasicUsers",
+    "Login: https://two4-mintep1-app-dev.onrender.com/api/v1/auth/login",
+    "Register: https://two4-mintep1-app-dev.onrender.com/api/v1/auth/register",
+    "Get All Users: https://two4-mintep1-app-dev.onrender.com/api/v1/users/all",
+    "Get/Put/Del User by ID: https://two4-mintep1-app-dev.onrender.com/api/v1/users/:uuid",
+    "Create Quiz: https://two4-mintep1-app-dev.onrender.com/api/v1/quizzes/create",
+    "Get/Del/Put Quiz: https://two4-mintep1-app-dev.onrender.com/api/v1/quizzes/:id",
+    "Create Score: https://two4-mintep1-app-dev.onrender.com/api/v1/quizzes/scores",
+    "Create Participation: https://two4-mintep1-app-dev.onrender.com/api/v1/quizzes/participation",
+    "Get User Answers: https://two4-mintep1-app-dev.onrender.com/api/v1/quizzes/userAnswers",
+    "Create User Answers: https://two4-mintep1-app-dev.onrender.com/api/v1/quizzes/createAnswers",
 
-// Export the router
+  ]
+  res.send(x)
+})
+
 export default router;
